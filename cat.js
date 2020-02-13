35ebcfdb820d3d2096883695d4e37a0b452d658c
hoangson763763 <hoangson763763@gmail.com>
2020-02-13 10:23:54 +0700

    cat eat mouse

--- cat.js
+++ cat.js
@@ -1,3 +1,14 @@
-function Cat(){
-	
+function Cat(name){
+	this.name = name;
+	this.stomach = [];
 }
+Cat.prototype.eat = function(mouse){
+	this.stomach.push(mouse);
+}
+function Mouse(name){
+	this.name = name
+}
+cat = new Cat('Tom');
+mouse = new Mouse('jerry')
+cat.eat(mouse);
+console.log(cat);
